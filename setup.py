721089bfa4fb6316344b41355a1a0bf9611e96a4
Joe Block <jpb@unixorn.net>
2015-11-10 19:17:50 -0800

    Add author, author_email & url to make pypi happy

--- setup.py
+++ setup.py
@@ -10,10 +10,13 @@ requirements = map(str.strip, open("requirements.txt").readlines())
 
 setup(
   name = name,
+  author = "Joe Block",
+  author_email = "jpb@unixorn.net",
   description = "Haze AWS utility functions",
+  url = "https://github.com/unixorn/haze",
   packages = find_packages(),
-  version = "0.0.4",
-  download_url = 'https://github.com/unixorn/haze/tarball/0.0.4',
+  version = "0.0.5",
+  download_url = 'https://github.com/unixorn/haze/tarball/0.0.5',
   keywords = ['aws', 'cloud'],
   install_requires = requirements,
   entry_points = {

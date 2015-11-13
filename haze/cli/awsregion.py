a1b88dade4a63494d2c90ae929c522c0170b9100
Joe Block <jpb@unixorn.net>
2015-11-13 11:51:41 -0800

    Cleanups + add haze-aws-metadata entry point
    * Add shebangs
    * Add haze-aws-metadata entry point
    * State deps in setup.py

---
+++ haze/cli/awsregion.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright (c) 2015 Joe Block <jpb@unixorn.net>
+#
+# This code is released under the Apache 2.0 license
+# See the LICENSE file in this repository for details
+#
+# I have often found it convenient to be able to read what AWS region
+# an instance is in inside bash scripts.
+
+from haze.ec2 import myRegion
+
+# Bash helpers
+def awsMyRegion():
+  """Print a running instance's AWS region"""
+  print myRegion()
+
+if __name__ == "__main__":
+  awsMyRegion()
--- setup.py
+++ setup.py
@@ -6,23 +6,25 @@
 from setuptools import setup, find_packages
 
 name = "haze"
-requirements = map(str.strip, open("requirements.txt").readlines())
 
 setup(
   name = name,
   author = "Joe Block",
   author_email = "jpb@unixorn.net",
-  description = "Haze AWS utility functions",
+  description = "Haze is a collection of AWS utility functions",
   url = "https://github.com/unixorn/haze",
   packages = find_packages(),
-  version = "0.0.5",
-  download_url = 'https://github.com/unixorn/haze/tarball/0.0.5',
+  install_requires = [
+    "boto==2.38.0"
+  ],
+  version = "0.0.8",
+  download_url = 'https://github.com/unixorn/haze/tarball/0.0.8',
   keywords = ['aws', 'cloud'],
-  install_requires = requirements,
   entry_points = {
     "console_scripts": [
-      "aws-instance-id = %s.commands.myinstanceid:awsInstanceID" % name,
-      ("aws-region = %s.commands.myregion:awsMyRegion" % name)
+      "haze-aws-instance-id = %s.cli.awsinstanceid:awsInstanceID" % name,
+      "haze-aws-read-metadata-key = %s.cli.awsmetadata:awsReadMetadataKey" % name,
+      "haze-aws-region = %s.cli.awsregion:awsMyRegion" % name
     ]
   }
 )
